e650a30853ebade39705ed7a95ced8f187dab507
Nico Martin <mail@nicomartin.ch>
2021-07-25 21:27:36 +0200

    ready for the Control UI

--- main.js
+++ main.js
@@ -3,23 +3,27 @@ const bluetoothService = require("./src/bluetooth.js");
 const utils = require("./src/utils.js");
 
 const run = async () => {
-  bluetoothService();
-
-  return;
   console.log("starting..");
   
   const mc = await motorControl();
   console.log("motor started");
 
   mc.left(50);
-  mc.right(100);
-  await utils.wait(2000);
-
-  mc.left(-50);
-  mc.right(-100);
-  await utils.wait(2000);
-
+  mc.right(50);
+  await utils.wait(50);
   mc.stop();
+  
+  bluetoothService((cmd, speed = 0) => {
+    if(cmd === "stop") {
+      mc.stop();
+    } else if(cmd === "l") {
+      mc.left(speed);
+    } else if(cmd === "r") {
+      mc.right(speed);
+    } else {
+      console.log("ERROR: invalid command");
+    }
+  });
 };
 
 run();
--- src/bluetooth.js
+++ src/bluetooth.js
@@ -1,61 +1,62 @@
-process.env["BLENO_DEVICE_NAME"] = "Nico Car";
+process.env["BLENO_DEVICE_NAME"] = "Nicos Web BLE Car";
 
 const bleno = require("bleno");
 const Characteristic = bleno.Characteristic;
 
-const exampleService = {
-  uuid: "fff6",
-  characteristics: [
-    new Characteristic({
-      uuid: "fff7",
-      properties: ["write"],
-      descriptors: [
-        new bleno.Descriptor({
-          uuid: "2901",
-          value: "My Test"
-        })
-      ],
-      onWriteRequest: (data, offset, withoutResponse, callback) => {
-        data = data.toString();
-        if(data === "stop"){
-          console.log("STOP THE ENGINE!");
+const bluetoothService = (motorCallback) => {
+
+  const motorControlService = {
+    uuid: "fff1",
+    characteristics: [
+      new Characteristic({
+        uuid: "fff2",
+        properties: ["write"],
+        descriptors: [
+          new bleno.Descriptor({
+            uuid: "fff3",
+            value: "motorControl Characteristic. Expects \"{'l'|'r'|'stop'}:{-100 - 100}\""
+          })
+        ],
+        onWriteRequest: (data, offset, withoutResponse, callback) => {
+          data = data.toString();
+          if(data === "stop"){
+            motorCallback("stop");
+            callback("success");
+            return;
+          }
+
+          const parts = data.split(":");
+          if(parts.length !== 2){
+            console.log("ERROR: invalid input data");
+            callback("error");
+            return;
+          }
+
+          const position = parts[0];
+          if(position !== "l" && position !== "r"){
+            console.log("ERROR: first value has to be r or l");
+            callback("error");
+            return;
+          }
+
+          const speed = parseInt(parts[1]);
+          if(isNaN(speed) || speed < -100 || speed > 100){
+            console.log("ERROR: second value has to be between -100 and 100");
+            callback("error");
+            return;
+          }
+
+          motorCallback(position, speed);
           callback("success");
-          return;
-        }
+        },
+      })
+    ]
+  };
 
-        const parts = data.split(":");
-        if(parts.length !== 2){
-          console.log("ERROR: invalid input data");
-          callback("error");
-          return;
-        }
-
-        const position = parts[0];
-        if(position !== "l" && position !== "r"){
-          console.log("ERROR: first value has to be r or l");
-          callback("error");
-          return;
-        }
-
-        const speed = parseInt(parts[1]);
-        if(isNaN(speed) || speed < -100 || speed > 100){
-          console.log("ERROR: second value has to be between -100 and 100");
-          callback("error");
-          return;
-        }
-
-        console.log(`should turn ${position === "l" ? "left" : "right"} wheel width speed ${speed}%`);
-        callback("success");
-      },
-    })
-  ]
-};
-
-const bluetoothService = () => {
   bleno.on("stateChange", function(state) {
     if(state === "poweredOn"){
-      console.log("Bluetooth poweredOn");
-      bleno.startAdvertising("nicosRpi", [exampleService.uuid]);
+      console.log("Bluetooth started");
+      bleno.startAdvertising("WebBluetoothCar", [motorControlService.uuid]);
     } else {
       bleno.stopAdvertising();
     }
@@ -68,7 +69,7 @@ const bluetoothService = () => {
     }
   
     bleno.setServices([
-      exampleService
+      motorControlService
     ]);
   });
 };
