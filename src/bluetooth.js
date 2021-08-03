dc4804866e2794d85b517bb28d848e4d1932c2d6
Nico Martin <mail@nicomartin.ch>
2021-08-03 13:28:15 +0200

    added the right UUIDs for device information

--- src/bluetooth.js
+++ src/bluetooth.js
@@ -9,10 +9,10 @@ const bluetoothService = async (leftWheel, rightWheel) => {
   const serial = await deviceInfo.deviceSerial();
 
   const deviceInfoService = {
-    uuid: "0x180a",
+    uuid: "180a",
     characteristics: [
       new Characteristic({
-        uuid: "0x2a25",
+        uuid: "2a25",
         properties: ["read"],
         value: new Buffer(pkg.version),
         descriptors: [
@@ -23,7 +23,7 @@ const bluetoothService = async (leftWheel, rightWheel) => {
         ],
       }),
       new Characteristic({
-        uuid: "0x2a27",
+        uuid: "2a27",
         properties: ["read"],
         value: new Buffer(serial),
         descriptors: [
