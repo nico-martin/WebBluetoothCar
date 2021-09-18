05e3573e64a11d0f295af35f64a17792902faae4
Nico Martin <mail@nicomartin.ch>
2021-09-18 12:30:13 +0200

    added light feedback

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const motorControl = require("./src/motorControl.js");
 const bluetoothService = require("./src/bluetooth.js");
 const utils = require("./src/utils.js");
-const { scrollText } = require("matrix11x7");
+const { scrollText, basicMatrix } = require("matrix11x7");
+
+const ledMatrix = {
+  go: [
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+    [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200],
+  ],
+  stop: [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ],
+};
 
 const run = async () => {
   console.log("starting..");
@@ -9,7 +30,7 @@ const run = async () => {
   const mc = await motorControl();
 
   // greetings
-  scrollText(`Hello I m ready`, {
+  scrollText("Ready", {
     infinite: false,
   });
 
@@ -17,9 +38,11 @@ const run = async () => {
   await bluetoothService(({ speedLeft = 0, speedRight = 0 }) => {
     if (speedLeft === 0 || speedRight === 0) {
       mc.stop();
+      basicMatrix(ledMatrix.stop);
     } else {
       mc.left(speedLeft);
       mc.right(speedRight);
+      basicMatrix(ledMatrix.go);
     }
   });
 
