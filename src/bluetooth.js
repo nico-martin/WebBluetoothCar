dc22546a45c4e7c83b4eb6db0080c10ff2f4d6ad
Nico Martin <mail@nicomartin.ch>
2021-10-31 22:59:06 +0100

    debugging

--- src/bluetooth.js
+++ src/bluetooth.js
@@ -9,19 +9,17 @@ const bluetoothService = async (move, onBatteryUpdate) => {
   const battery = batteryService(onBatteryUpdate);
   const device = await deviceInfoService();
   const motorControl = motorControlService(move);
+  console.log(motorControl);
 
-  bleno.on('stateChange', (state) => {
-    console.log('stateChange', state);
-    if (state === 'poweredOn') {
-      bleno.startAdvertising('SpeedWheels', [
-        battery.uuid,
-        device.uuid,
-        motorControl.uuid,
-      ]);
-    } else {
-      bleno.stopAdvertising();
-    }
-  });
+  bleno.on('stateChange', (state) =>
+    state === 'poweredOn'
+      ? bleno.startAdvertising('SpeedWheels', [
+          battery.uuid,
+          device.uuid,
+          motorControl.uuid,
+        ])
+      : bleno.stopAdvertising()
+  );
 
   bleno.on('advertisingStart', (err) => {
     if (err) {
